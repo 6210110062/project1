1516d424ed2ca3a4b0997965b2c8c295d28028f7
062 <6210110062@psu.ac.th>
2022-09-29 00:28:38 +0700

    fizzbuzz

---
+++ tests/test_FizzBuzz.py
@@ -0,0 +1,34 @@
+from coe_number.FizzBuzz_utils import fizzbuzz
+import unittest
+
+
+class primelisttest(unittest.TestCase):
+    def test_give_3_is_fizz(self):
+        num = 3
+        result = fizzbuzz(num)
+        self.assertEqual(result, 'Fizz')
+
+    def test_give_21_is_fizz(self):
+        num = 21
+        result = fizzbuzz(num)
+        self.assertEqual(result, 'Fizz')
+
+    def test_give_5_is_buzz(self):
+        num = 5
+        result = fizzbuzz(num)
+        self.assertEqual(result, 'Buzz')
+
+    def test_give_100_is_buzz(self):
+        num = 100
+        result = fizzbuzz(num)
+        self.assertEqual(result, 'Buzz')
+
+    def test_give_30_is_fizzbuzz(self):
+        num = 30
+        result = fizzbuzz(num)
+        self.assertEqual(result, 'FizzBuzz')
+
+    def test_give_225_is_fizzbuzz(self):
+        num = 225
+        result = fizzbuzz(num)
+        self.assertEqual(result, 'FizzBuzz')
--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -8,17 +8,17 @@ class PrimeListTest(unittest.TestCase):
         is_prime = is_prime_list(prime_list)
         self.assertTrue(is_prime)
 
-    def test_give_float_zero_2_3_4_is_prime(self):
+    def test_give_2_3_4_is_not_prime(self):
         prime_list = [2, 3, 4]
         is_prime = is_prime_list(prime_list)
         self.assertFalse(is_prime)
 
-    def test_give_4_5_6_is_prime(self):
+    def test_give_4_5_6_is_not_prime(self):
         prime_list = [4, 5, 6]
         is_prime = is_prime_list(prime_list)
         self.assertFalse(is_prime)
 
-    def test_give_float_2_3_4_is_prime(self):
+    def test_give_float_3_5_7_is_prime(self):
         prime_list = [3, 5, 7]
         is_prime = is_prime_list(prime_list)
         self.assertTrue(is_prime)
